db9be9fbbb087510bb44ff69cdfe7249e744e2b8
deepanshugupta99 <41442287+deepanshugupta99@users.noreply.github.com>
2020-06-12 12:08:58 +0530

    Add files via upload

---
+++ imagE.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Untitled Document</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+<div id="carousel1" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel1" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel1" data-slide-to="1"></li>
+	    <li data-target="#carousel1" data-slide-to="2"></li>
+  </ol>
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active"><img src="images/Carousel_Placeholder.png" alt="First slide image" class="center-block">
+	      <div class="carousel-caption">
+	        <h3>First slide Heading</h3>
+	        <p>First slide Caption</p>
+          </div>
+        </div>
+	    <div class="item"><img src="images/Carousel_Placeholder.png" alt="Second slide image" class="center-block">
+	      <div class="carousel-caption">
+	        <h3>Second slide Heading</h3>
+	        <p>Second slide Caption</p>
+          </div>
+        </div>
+	    <div class="item"><img src="images/Carousel_Placeholder.png" alt="Third slide image" class="center-block">
+	      <div class="carousel-caption">
+	        <h3>Third slide Heading</h3>
+	        <p>Third slide Caption</p>
+          </div>
+        </div>
+  </div>
+	  <a class="left carousel-control" href="#carousel1" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#carousel1" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>
+	
+	
+<img src="images/ImgResponsive_Placeholder.png" class="img-circle img-responsive" alt="Placeholder image">
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
