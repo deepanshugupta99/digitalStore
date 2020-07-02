90ddf9daeaa7273ace0ee888238279bda9e57af5
deepanshugupta99 <41442287+deepanshugupta99@users.noreply.github.com>
2020-07-02 19:06:09 +0530

    Add files via upload

---
+++ seller/conn.php
@@ -0,0 +1,23 @@
+<?php
+$uname='grocease';
+$pass='Genuine@2017';
+$host='43.255.152.5';
+$db='grocease1';
+/*
+$uname='epiz_22891465';
+$pass='daHfAFdR';
+$host='sql102.epizy.com';
+$db='epiz_22891465_dgcart';*/
+//$query=mysqli_connect($host,$uname,$pass,$db);
+$query=mysqli_connect("localhost","root","","grocease1");
+if($query==true)
+{
+
+}
+else
+{ echo "Fail";}
+?>
+
+
+
+
