503e74ea9d559d449bf10b53506cd1bd11561ad0
deepanshugupta99 <41442287+deepanshugupta99@users.noreply.github.com>
2020-07-04 23:45:03 +0530

    Add files via upload

---
+++ seller/dynamic-pages/allpages.php
@@ -0,0 +1,5 @@
+<ul class="nav navmenu-nav"> 
+<li><a href="">Home V1</a></li>
+<li><a href="">Home Page V2</a></li>
+<li><a href="">Home Page V3</a></li>
+</ul>
---
+++ seller/dynamic-pages/footer.php
@@ -0,0 +1,121 @@
+<footer>
+
+<section class="footer-Content">
+<div class="container">
+<div class="row">
+<div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="0">
+<div class="widget">
+<h3 class="block-title">About us</h3>
+<div class="textwidget">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis tincidunt est, et euismod purus suscipit quis. Etiam euismod ornare elementum. Sed ex est, consectetur eget facilisis sed, auctor ut purus.</p>
+</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="0.5">
+<div class="widget">
+<h3 class="block-title">Useful Links</h3>
+<ul class="menu">
+<li><a href="#">Home</a></li>
+<li><a href="#">Categories</a></li>
+<li><a href="#">FAQ</a></li>
+<li><a href="#">Left Sidebar</a></li>
+<li><a href="#">Pricing Plans</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Contact</a></li>
+<li><a href="#">Full Width Page</a></li>
+<li><a href="#">Terms of Use</a></li>
+<li><a href="#">Privacy Policy</a></li>
+</ul>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="1s">
+<div class="widget">
+<h3 class="block-title">Latest Tweets</h3>
+<div class="twitter-content clearfix">
+<ul class="twitter-list">
+<li class="clearfix">
+<span>
+Platform to Download and Submit #Bootstrap Templates via @ProductHunt @GrayGrids
+<a href="#">http://t.co/cLo2w7rWOx</a>
+</span>
+</li>
+<li class="clearfix">
+<span>
+Introducing Bootstrap 4 Features: What’s new, What’s gone!
+<a href="#">http://t.co/cLo2w7rWOx</a>
+</span>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-delay="1.5s">
+<div class="widget">
+<h3 class="block-title">Random Ads</h3>
+<ul class="featured-list">
+<li>
+<img alt="" src="assets/img/featured/img1.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+<li>
+<img alt="" src="assets/img/featured/img2.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+<li>
+<img alt="" src="assets/img/featured/img3.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+<li>					
+<img alt="" src="assets/img/featured/img4.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+<li>
+<img alt="" src="assets/img/featured/img5.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+<li>
+<img alt="" src="assets/img/featured/img6.jpg">
+<div class="hover">
+<a href="#"><span>$49</span></a>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<div id="copyright">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="site-info pull-left">
+<p>All copyrights reserved &copy; 2018 - Designed by <a href="" rel="nofollow">Deepanshu Gupta</a></p>
+</div>
+<div class="bottom-social-icons social-icon pull-right">
+<a class="facebook" target="_blank" href="https://web.facebook.com/"><i class="fa fa-facebook"></i></a>
+<a class="twitter" target="_blank" href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+<a class="dribble" target="_blank" href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a>
+<a class="flickr" target="_blank" href="https://www.flickr.com/"><i class="fa fa-flickr"></i></a>
+<a class="youtube" target="_blank" href="https://youtube.com/"><i class="fa fa-youtube"></i></a>
+<a class="google-plus" target="_blank" href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a>
+<a class="linkedin" target="_blank" href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</footer>
